7c1673963359c3c203b8b720e8ab45c8eec291d7
Dacordon <daniel.camacho.mt@correo.usa.edu.co>
2021-10-18 14:07:18 -0500

    se ajustan detalles para acomodar la salida segun mastertech

--- src/main/java/com/reto3proyecto/reto3p/entities/Client.java
+++ src/main/java/com/reto3proyecto/reto3p/entities/Client.java
@@ -25,19 +25,19 @@ public class Client implements Serializable{
  @Id 
  @GeneratedValue (strategy=GenerationType.IDENTITY)
     private Integer idClient;
-    private String name;
     private String email;
     private String password;
+    private String name;
     private int age;
-
-    @OneToMany(cascade = {CascadeType.PERSIST},mappedBy = "client")
-    @JsonIgnoreProperties("client")
-    private List<Reservation> reservations;
     
     @OneToMany(cascade = {CascadeType.PERSIST},mappedBy = "client")
     @JsonIgnoreProperties("client")
     private List<Message> messages;
 
+    @OneToMany(cascade = {CascadeType.PERSIST},mappedBy = "client")
+    @JsonIgnoreProperties("client")
+    private List<Reservation> reservations;
+    
     
     public List<Message> getMessages() {
         return messages;
